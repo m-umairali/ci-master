a6f3759ca4aacbace511f77557e756c1cf6817b2
umair <umairalism@gmail.com>
2023-12-15 22:39:37 +0500

    apple mispled

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
 
-fruits = ['apple', 'blueberry', 'cherry', 'orange']
+fruits = ['applee', 'blueberry', 'cherry', 'orange']
 counts = [140, 10, 30, 55]
 bar_labels = ['red', 'blue', '_red', 'orange']
 bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
